506e5d641372ec329d6d91be52ca38a78b3450d2
Catarina Nascimento <55981340+ParkKatrin@users.noreply.github.com>
2020-03-13 16:32:49 +0000

    Add files via upload

--- distancia_fe3.py
+++ distancia_fe3.py
@@ -3,66 +3,99 @@ import MDAnalysis as mda
 import numpy as np
 
 
-def create_universe(gro,xtc):
-    universe = mda.Universe(gro,xtc)
+def plotter_alpha(distances, pos):
+    print(distances)
+    print(pos)
+    import os
+    os.makedirs("Distancias", exist_ok=True)
+    import matplotlib.pyplot as plt
+    ax = plt.subplot()
+    ax.plot(distances[:, 0], distances[:, 1], 'r--', lw=2, label=r"$R_G$")
+    ax.set_xlabel("time (ps)")
+    ax.set_ylabel(r"Pair distance ($\AA$)")
+    # ax.set_ylim(0, 5)
+    # ax.hlines(2.8, 0, 1000)
+    ax.set_title("Distancias " + str(acid[0].resname) + "( " + str(acid[0].resid) + " ) - " + str(
+        basic[pos].resname) + " (" + str(basic[pos].resid) + " )")
+    title = "Distancias/" + str(acid[0].resname) + "(" + str(acid[0].resid) + ")-" + str(
+        basic[pos].resname) + "(" + str(basic[pos].resid) + ")"
+    ax.figure.savefig(title)
+    plt.draw()  # draws graph in the file
+    plt.cla()  # clears the axis for next use
+
+
+def create_universe(gro, xtc):
+    universe = mda.Universe(gro, xtc)
     return universe
 
-def tipo_universo (universe):
 
+def tipo_universo(universe):
+    global acid, basic
     tentativa_max_ddistances = 2.8
 
     acid = universe.select_atoms(
-        "(resname FE3 ) ")  #criação do universo com FE3
-
+        "(resname FE3 ) ")  # criação do universo com FE3
 
     basic = universe.select_atoms(
-        "not resname SOL or not name C* or not name H*") #criação do universo os restantes residuos
+        "not resname SOL or not name C* or not name H*")  # criação do universo os restantes residuos
 
     return tentativa_max_ddistances, acid, basic
 
 
-def criacao_ficheiro(tentativa_max_ddistances, acid, basic):
-    lista = []
-    resids = []
-    resname = []
-
+def criacao_ficheiro(tentativa_max_ddistances, acid, basic, universe):
     with open('Saltbridges_acid_list.txt', 'w+') as ficheiro:
-
         for posicao in range(len(basic)):
-            distance = np.linalg.norm(acid[0].position - basic[posicao].position) #distancia de posições
+            distance = np.linalg.norm(
+                acid[0].position - basic[posicao].position)  # distancia de posições
             if distance < tentativa_max_ddistances and distance != 0:
-                lista.append(distance)
+                lista = []
+                resids = []
+                resname = []
+                distances = []
+                frase = str(acid[0].resname) + ' ' + str(acid[0].resid) + ' - ' + str(
+                    basic[posicao].resname) + ' ' + str(basic[posicao].resid) + ' Distance: ' + str(
+                    distance)
+                print(frase)
+                ficheiro.write(frase + '\n')
                 resids.append(basic[posicao].resid)
                 resname.append(basic[posicao].resname)
-                frase = str(acid[0].resname) + ' ' + str(acid[0].resid) + ' - ' + str(basic[posicao].resname) + ' ' + str(basic[posicao].resid) + ' Distance: ' + str(distance)
-                ficheiro.write(frase + '\n')
-    ficheiro.close()
-    return  resids,resname
+                for ts in universe.trajectory:
+                    distances.append((universe.trajectory.time, np.linalg.norm(
+                        acid[0].position - basic[posicao].position)))
+                    # distancia = universe.trajectory.time, acid[0].position - basic[posicao].position
+                    lista.append(distances)
+                    # lista = np.array(lista)
+                    # print(distances)
+                distances = np.array(distances)
+                plotter_alpha(distances, posicao)
 
-def cenas(universe,acid, resids):
+    ficheiro.close()
+    return resids, resname
+
+
+def cenas(universe, acid, resids):
     with open('Distancias.txt', 'w+') as file:
         listsa = []
         for i in resids:
             i = str(i)
-            frase = "(resid "+ i +" )"
+            frase = "(resid " + i + " )"
             atom = universe.select_atoms(frase)
+            for ts in universe.trajectory:
+                listsa.append((universe.trajectory.time, np.linalg.norm(
+                    acid[0].position - atom[0].position)))
             dis = np.linalg.norm(acid[0].position - atom[0].position)  # distancia de posições
-            listsa.append(dis)
+            # listsa.append(dis)
             frase = str(acid[0].resname) + ' ' + str(acid[0].resid) + ' - ' + str(
                 atom[0].type) + ' ' + str(
                 atom[0].resid) + ' Distance: ' + str(dis)
-            file.write(frase + '\n')
+        plotter_alpha(listsa, i)
+        file.write(frase + '\n')
     file.close()
-
-
-
 
 
 if __name__ == '__main__':
     universo = create_universe('./protein_md001_A_2/md2.gro',
                                './protein_md001_A_2/md2.xtc')
     valor, acidos, basicos = tipo_universo(universo)
-    resids, resnames = criacao_ficheiro(valor, acidos, basicos)
-    cenas(universo,acidos, resids)
-
-
+    resids, resnames = criacao_ficheiro(valor, acidos, basicos, universo)
+    # cenas(universo,acidos, resids)
