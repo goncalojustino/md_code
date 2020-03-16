6f9d767165b212e4c5ccd3c30cc33b9b492f3b7e
Catarina Nascimento <55981340+ParkKatrin@users.noreply.github.com>
2020-03-16 19:24:49 +0000

    Criação de função para um novo ficheiro
    Neste novo upload criei a função que irá criar um novo ficheiro com as linhas do ficheiro .gro.

--- katie/FE3_distances/distancia_fe3.py
+++ katie/FE3_distances/distancia_fe3.py
@@ -1,4 +1,5 @@
 ##IMPORTS##
+import os
 import statistics as sta
 
 import MDAnalysis as mda
@@ -10,10 +11,6 @@ def plotter_alpha(distances, pos, media):
     com os parametros feitos antriormente.
     Estes gráficos irão ter as distancias ao longo do tempo do movimento da ligação indicada no titulo da imagem
     Irá ter também uma linha que representa a média das distancias'''
-    # print(distances)
-    # print(len(distances))
-    # print(pos)
-    import os
     os.makedirs("Distancias33", exist_ok=True)
     import matplotlib.pyplot as plt
     # zorder: marca a prioridade entre gráficos, quanto maior o numero mais a frente fica do ecra
@@ -29,7 +26,8 @@ def plotter_alpha(distances, pos, media):
         basic[pos].resname) + " ( " + str(basic[pos].resid) + " )")
     title = "Distancias33/" + str(ferro[0].resname) + "(" + str(ferro[0].resid) + ")-" + str(
         basic[pos].resname) + "(" + str(basic[pos].resid) + ")-" + str(media).replace(".",
-                                                                                      ",")  # havia graficos a
+                                                                                      ",")
+    # havia graficos a
     plt.savefig(title)
     plt.draw()  # draws graph in the file
     plt.cla()  # clears the axis for next use
@@ -54,23 +52,22 @@ def tipo_universo(universe):
     return tentativa_max_ddistances, ferro, basic
 
 
-def criacao_ficheiro(tentativa_max_ddistances, ferro, basic, universe):
-    '''Irá criar um ficheiro que terá o ferro ligado a um outro atomo do universo com a distancia desejada'''
-    with open('Saltbridges_ferro_list.txt', 'w+') as ficheiro:
+def ligações(tentativa_max_ddistances, ferro, basic, universe):
+    resids = []
+    resname = []
+    name = []
+    '''Irá criar um ficheiro que terá o ferro ligado a um outro atomo do universo com a 
+    distancia desejada'''
+    with open('Ligações_com_FE3.txt', 'w+') as ficheiro:
         for posicao in range(len(basic)):
             distance = np.linalg.norm(
                 ferro[0].position - basic[posicao].position)  # distancia de posições
-            if distance < tentativa_max_ddistances and distance != 0:  # Irá definoir dizerq eu a distancia entre os atmos terá de quer menor que a distancia que tem no espaço, se isso falhar, então será 'descartada' essa opção
+            if distance < tentativa_max_ddistances and distance != 0:  # Irá definoir dizerq eu a
+                # distancia entre os atmos terá de quer menor que a distancia que tem no espaço, se
+                # isso falhar, então será 'descartada' essa opção
                 lista = []
-                resids = []
-                resname = []
-                name = []
                 distances = []
                 valores = []
-                resids.append(basic[posicao].resid)
-                resname.append(basic[posicao].resname)
-                name.append(basic[posicao].name)
-                print(name)
                 for ts in universe.trajectory:
                     distances.append((universe.trajectory.time, np.linalg.norm(
                         ferro[0].position - basic[
@@ -80,43 +77,53 @@ def criacao_ficheiro(tentativa_max_ddistances, ferro, basic, universe):
                 media = sta.mean(valores)  # média que será colocada no gráfico
                 frase = str(ferro[0].resname) + ' ' + str(ferro[0].resid) + ' ' + str(
                     ferro[0].name) + ' - ' + str(basic[posicao].resname) + ' ' + str(
-                    basic[posicao].resid) + ' ' + str(basic[posicao].name) + ' -- Distancia: ' + str(
+                    basic[posicao].resid) + ' ' + str(
+                    basic[posicao].name) + ' -- Distancia: ' + str(
                     distance) + ' [Media = ' + str(media) + ']'
                 ficheiro.write(frase + '\n')
                 distances = np.array(distances)
                 plotter_alpha(distances, posicao, media)
-                print('plot feito')
+                resids.append(basic[posicao].resid)
+                resname.append(basic[posicao].resname)
+                name.append(basic[posicao].name)
 
     ficheiro.close()
-    return resids, resname
+    return resids, resname, name
 
 
-# def cenas(universe, ferro, resids):
-#     with open('Distanciasas.txt', 'w+') as file:
-#         listsa = []
-#         for i in resids:
-#             i = str(i)
-#             frase = "(resid " + i + " )"
-#             atom = universe.select_atoms(frase)
-#             for ts in universe.trajectory:
-#                 listsa.append((universe.trajectory.time, np.linalg.norm(
-#                     ferro[0].position - atom[0].position)))
-#             dis = np.linalg.norm(ferro[0].position - atom[0].position)  # distancia de posições
-#             # listsa.append(dis)
-#             frase = str(ferro[0].resname) + ' ' + str(ferro[0].resid) + ' - ' + str(
-#                 atom[0].type) + ' ' + str(
-#                 atom[0].resid) + ' Distance: ' + str(dis)
-#         plotter_alpha(listsa, i)
-#         file.write(frase + '\n')
-#     file.close()
+def linhas_usadas_de_ficheiro(resids, resnames, names):
+    os.remove('Distanciasas1.txt')
+    chaves = []
+    for loc in range(len(resids)):
+        if len(names[loc]) == 2:
+            chave = ' ' + str(resids[loc]) + str(resnames[loc]) + '     ' + str(names[loc])
+            chaves.append(chave)
+        if len(names[loc]) == 3:
+            chave = ' ' + str(resids[loc]) + str(resnames[loc]) + '    ' + str(names[loc])
+            chaves.append(chave)
+        if len(names[loc]) == 1:
+            chave = ' ' + str(resids[loc]) + str(resnames[loc]) + '      ' + str(names[loc])
+            chaves.append(chave)
+    chaves.append('339FE3     FE')
+
+    with open('./md003/md3.gro') as f:
+        linhas = f.readlines()
+        for lines in linhas:
+            for pos in range(len(chaves)):
+                if chaves[pos] in lines:
+                    with open('Distanciasas1.txt', 'a+') as file:
+                        file.write(lines + '\n')
+                    file.close()
+    f.close()
 
 
 if __name__ == '__main__':
     # universo = create_universe('./protein_md001_A_2/md2.gro',
     #                            './protein_md001_A_2/md2.xtc')
 
-    universo = create_universe('./md003/md3.gro',
+    UNIVERSO = create_universe('./md003/md3.gro',
                                './md003/md3.xtc')
 
-    valor, ferroos, basicos = tipo_universo(universo)
-    resids, resnames = criacao_ficheiro(valor, ferroos, basicos, universo)
+    VALOR, FERROS, BASICOS = tipo_universo(UNIVERSO)
+    RESIDS, RESNAMES, NAMES = ligações(VALOR, FERROS, BASICOS, UNIVERSO)
+    linhas_usadas_de_ficheiro(RESIDS, RESNAMES, NAMES)
