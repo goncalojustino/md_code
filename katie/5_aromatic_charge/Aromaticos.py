13338210e05e792d49c6060c426e595ce91d0abc
Catarina Nascimento <55981340+ParkKatrin@users.noreply.github.com>
2020-04-01 17:15:52 +0100

    update e colocadas mais funções; alteração de dockstrings

--- katie/5_aromatic_charge/Aromaticos.py
+++ katie/5_aromatic_charge/Aromaticos.py
@@ -1,13 +1,21 @@
-from Look_for_point_charges import *
+import os
 import MDAnalysis as mda
 import numpy as np
-import os
+from numpy import linalg as la
+from Look_for_point_charges import dicti, temp
 
-
-centroid_list = []  #esta lista sera criada aqui para poder ser utilizada no codigo inteiro
+centroid_list = []  # esta lista sera criada aqui para poder ser utilizada no codigo inteiro
                     # sem a reeniciar
 
-def cut_off(distance,nome_arom,name_carga):
+def angulo_normal_centroid(distance, nome_arom, name_carga, normal, centroid):
+    angles = []
+    angles.append(np.rad2deg(np.arccos((np.dot(normal, centroid)) /
+                                       (la.norm(normal) * la.norm(centroid)))))
+    print('Aromatic Aminoacid:', nome_arom, ' - ', 'Charged Aminoacid:', name_carga, 'Distance:',
+          distance, ' Angle:', str(angles)[1:-1]+'°')
+
+
+def cut_off(distance, nome_arom, name_carga, normalVector, subtraction):
     '''
     Esta função irá verificar se a distancia é menor do que os parametros desejados e dar um print
     dos aminoacidos em que esta foi feita com o seu respetivo valor
@@ -15,8 +23,8 @@ def cut_off(distance,nome_arom,name_carga):
     :param nome_arom: é o nome do aminoacido aromatico utilizado
     :param name_carga: é o nome do aminoacido carregado utilizado
     '''
-    if distance < 3.0:
-        print(nome_arom,' - ', name_carga, 'distance:', distance)
+    if distance < 6.0:
+        angulo_normal_centroid(distance, nome_arom, name_carga, normalVector, subtraction)
 
 
 def procura(nume):
@@ -37,7 +45,7 @@ def procura(nume):
                 return name_carga
 
 
-def hi2(lista_centroides,nome_arom):
+def distances(lista_centroides, nome_arom, normalVector):
     '''
     cria duas listas para guardar as chaves e os valores dessas chaves para as poder utilizar, irá
     postriormente criar uma 'frase' que contem os elementos necessarios para cria um universo cada
@@ -60,11 +68,13 @@ def hi2(lista_centroides,nome_arom):
             name_carga = procura(num)
             if str(ind) != "[]":
                 if len(np.array(ind)) == 1:
-                    distance = np.linalg.norm(np.subtract(np.array(ind[0]), np.array(lista_centroides)))
-                    cut_off(distance,nome_arom,name_carga)
+                    subtraction = np.subtract(np.array(ind[0]), np.array(lista_centroides))
+                    distance = np.linalg.norm(subtraction)
+                    cut_off(distance, nome_arom, name_carga, normalVector, subtraction)
                 if len(np.array(ind)) == 2:
-                    distance = np.linalg.norm(np.subtract(np.array(ind[1]), np.array(lista_centroides)))
-                    cut_off(distance,nome_arom,name_carga)
+                    subtraction = np.subtract(np.array(ind[1]), np.array(lista_centroides))
+                    distance = np.linalg.norm(subtraction)
+                    cut_off(distance, nome_arom, name_carga, normalVector, subtraction)
 
 
 def eliminar_ficheiros_existentes(interactions):
@@ -83,7 +93,7 @@ def eliminar_ficheiros_existentes(interactions):
         coordinates_trp(interactions)
 
 
-def six_points(aromatic_coords,aminoacids):
+def six_points(aromatic_coords, aminoacids):
     '''
     Esta função irá tratar todos os aminoacidos que contem uma hexose adicionando a informação
     desejada a um ficheiro de outpur 'aromaticos.txt'
@@ -96,23 +106,25 @@ def six_points(aromatic_coords,aminoacids):
         vector1 = aromatic_coords[0] - aromatic_coords[5]
         vector2 = aromatic_coords[1] - aromatic_coords[4]
         vector3 = aromatic_coords[2] - aromatic_coords[3]
-        medioX = (aromatic_coords[0]+aromatic_coords[5])/2
-        medioY = (aromatic_coords[1]+aromatic_coords[4])/2
-        medioZ = (aromatic_coords[2]+aromatic_coords[3])/2
-        centroid = (medioX+medioY+medioZ)/3
+        medioX = (aromatic_coords[0] + aromatic_coords[5]) / 2
+        medioY = (aromatic_coords[1] + aromatic_coords[4]) / 2
+        medioZ = (aromatic_coords[2] + aromatic_coords[3]) / 2
+        centroid = (medioX + medioY + medioZ) / 3
         prod1 = np.cross(vector1, vector2)
         prod2 = np.cross(vector1, vector3)
         prod3 = np.cross(vector2, vector3)
         normalVector = (prod1 + prod2 + prod3) / 3
         nome = str(aminoacids.resid) + str(aminoacids.resname)
-        dicti2.update({'anel aromatico: ': aminoacids.resname, 'centroide ': centroid, 'normal vector' : normalVector})
+        dicti2.update({'anel aromatico: ': aminoacids.resname, 'centroide ': centroid,
+                       'normal vector': normalVector})
         dicti1.update({nome: dicti2})
         ficheiro.write(str(dicti1) + '\n')
-        hi2(centroid,nome)
+        distances(centroid, nome, normalVector)
+
     ficheiro.close()
 
 
-def five_points (aromatic_coords,aminoacids):
+def five_points(aromatic_coords, aminoacids):
     '''
     Esta função irá tratar todos os aminoacidos que contem uma pentose adicionando a informação desejada a um ficheiro de outpur 'aromaticos.txt'
     :param aromatic_coords: coordenadas dos aminoacidos necessarios e pertencentes a pentose
@@ -124,10 +136,10 @@ def five_points (aromatic_coords,aminoacids):
         vector1 = aromatic_coords[1] - aromatic_coords[3]
         vector2 = aromatic_coords[0] - aromatic_coords[2]
         vector3 = aromatic_coords[0] - ((aromatic_coords[2] + aromatic_coords[3]) / 2)
-        medioX = (aromatic_coords[1]+aromatic_coords[3])/2
-        medioY = (aromatic_coords[0]+aromatic_coords[2])/2
-        medioZ = (aromatic_coords[0]+((aromatic_coords[2] + aromatic_coords[3]) / 2))/2
-        centroid = (medioX+medioY+medioZ)/3
+        medioX = (aromatic_coords[1] + aromatic_coords[3]) / 2
+        medioY = (aromatic_coords[0] + aromatic_coords[2]) / 2
+        medioZ = (aromatic_coords[0] + ((aromatic_coords[2] + aromatic_coords[3]) / 2)) / 2
+        centroid = (medioX + medioY + medioZ) / 3
         # print("Centroid : ", (medioX+medioY+medioZ)/3)
         prod1 = np.cross(vector1, vector2)
         prod2 = np.cross(vector1, vector3)
@@ -135,16 +147,17 @@ def five_points (aromatic_coords,aminoacids):
         normalVector = (prod1 + prod2 + prod3) / 3
         # print("Vetor normal: ",normalVector, "residuo: ", aminoacids.resid, aminoacids.resname)
         nome = str(aminoacids.resid) + str(aminoacids.resname)
-        dicti2.update({'anel aromatico: ': aminoacids.resname , 'centroide ': centroid, 'normal vector' : normalVector})
+        dicti2.update({'anel aromatico: ': aminoacids.resname, 'centroide ': centroid,
+                       'normal vector': normalVector})
         dicti1.update({nome: dicti2})
         # print(dicti1)
         ficheiro.write(str(dicti1) + '\n')
-        hi2(centroid,nome)
+        distances(centroid, nome, normalVector)
     ficheiro.close()
     return centroid_list, nome
 
 
-def nine_points (aromatic_coords,aminoacids):
+def nine_points(aromatic_coords, aminoacids):
     '''
     Esta função irá tratar todos os aminoacidos que contem um eneágono adicionando a informação desejada a um ficheiro de outpur 'aromaticos.txt'
     :param aromatic_coords: coordenadas dos aminoacidos necessarios e pertencentes ao eneágono
@@ -159,7 +172,7 @@ def nine_points (aromatic_coords,aminoacids):
         medioX = (aromatic_coords[3] + aromatic_coords[7]) / 2
         medioY = (aromatic_coords[0] + aromatic_coords[8]) / 2
         medioZ = (aromatic_coords[1] + ((aromatic_coords[7] + aromatic_coords[8]) / 2)) / 2
-        centroid = (medioX+medioY+medioZ)/3
+        centroid = (medioX + medioY + medioZ) / 3
         # print("Centroid : ", (medioX + medioY + medioZ) / 3)
         prod1 = np.cross(vector1, vector2)
         prod2 = np.cross(vector1, vector3)
@@ -167,11 +180,12 @@ def nine_points (aromatic_coords,aminoacids):
         normalVector = (prod1 + prod2 + prod3) / 3
         # print("Vetor normal: ",normalVector, "residuo: ", aminoacids.resid, aminoacids.resname)
         nome = str(aminoacids.resid) + str(aminoacids.resname)
-        dicti2.update({'anel aromatico: ': aminoacids.resname , 'centroide ': centroid, 'normal vector' : normalVector})
+        dicti2.update({'anel aromatico: ': aminoacids.resname, 'centroide ': centroid,
+                       'normal vector': normalVector})
         dicti1.update({nome: dicti2})
         # print(dicti1)
         ficheiro.write(str(dicti1) + '\n')
-        hi2(centroid,nome)
+        distances(centroid, nome, normalVector)
     ficheiro.close()
 
 
@@ -182,6 +196,7 @@ def create_universe(gro, xtc):
     :param xtc: ficheiro .xtc de input
     :return: o universo criado
     '''
+    global universe
     universe = mda.Universe(gro, xtc)
     return universe
 
@@ -195,8 +210,8 @@ def tipo_universo(universe):
     global interactions
 
     interactions = universe.select_atoms("(resname PHE) or (resname TRP)  or (resname TYR)"
-                                     " or (resname TYO) or (resname HIS) or (resname HSD)"
-                                     " or (resname HSE) or (resname HSP) or (resname HIT)").residues
+                                         " or (resname TYO) or (resname HIS) or (resname HSD)"
+                                         " or (resname HSE) or (resname HSP) or (resname HIT)").residues
     return interactions
 
 
@@ -224,7 +239,7 @@ def coordinates_phe_tyr_tyo(interactions):
                 coordenadas.append(ce1)
                 coordenadas.append(ce2)
                 coordenadas.append(cz)
-                six_points(coordenadas,aa)
+                six_points(coordenadas, aa)
 
 
 def coordinates_his_hsd_hse_hsp_hit(interactions):
@@ -320,13 +335,5 @@ if __name__ == '__main__':
 
     eliminar_ficheiros_existentes(INTERACTIONS)
 
-    ###################################################
     lista_centroides = []
     lista_centroides.append(centroid_list)
-    # hi(lista_centroides)
-    # print(lista_centroides)
-    ###################################################
-
-
-
-
