09c00dc5f4a47ba9c550f77407c8e31cb06c30fe
Catarina Nascimento <55981340+ParkKatrin@users.noreply.github.com>
2020-04-20 15:51:46 +0100

    adição de docstrings e alteração de nomes de variaveis
    adição de docstrings a explicar partes especificas do código e alteração de nomes de variaveis para algo mais percetivel

--- katie/5_aromatic_charge/Look_for_point_charges.py
+++ katie/5_aromatic_charge/Look_for_point_charges.py
@@ -1,166 +1,176 @@
+#IMPORTS
 import MDAnalysis as mda
 
 universe = mda.Universe("./md003/md3.gro", "./md003/md3.xtc")
+
+
 interactions = universe.select_atoms("(resname ARG and name NH1) or (resname ASP and name OD*) "
                                      "or (resname GLU and name OE*) or (resname HIS and name ND1)"
                                      "or (resname HIT and name NE2) or (resname LYS* and name NZ)"
                                      "or (resname TYO and name OH)")
-ferro = universe.select_atoms("(resname FE3 and name FE) or (resname V3 and name V3)"
+
+iron = universe.select_atoms("(resname FE3 and name FE) or (resname V3 and name V3)"
                               "or (resname VO and name V4) or (resname VO2 and name V5)"
                               "or (resname VO4 and name V5)")
-lista = []  # criação de uma lista vazia
+
+
+my_list = []
 ids = []
-dicti = {}  # criação de um dicionário
+dicti = {}
 
-# Seleção dos NH1 das ARG
 
-for line in interactions:  # correr por todas as interactions obtendo cada linha
-    if 'NH1' in str(line):  # verificar de o atmo que pretendemos encontra-se
-        frase = "resid " + str(line.resid) + " and (name HH11 or name HH12)"  ## criação de uma variavel para se poder criar uma seleção de um universo 'interativo'
-        select = universe.select_atoms(frase)  ## criação de uma nova seleção no universo utilizando a variavel definida acima
-        if str(select) != "<AtomGroup []>":  # esta funcao ira verificar se o HH12/HH11 existe, e se isso acontecer mante-lo
-            juncao = str(line.resid) + str(line.resname)    ##irá concatenar os id's e os nomes dos residuos em questão
+"""
+This function will create a dictionary that will contain as key all the atoms that are considered
+essential, as they are characteristic due to their load, and as key the name and id of all
+amino acids that meet the requirements, that is, it has a loaded atom in its composition
+"""
+# Selection of NH1 from ARG
+for line in interactions:
+    if 'NH1' in str(line):
+        phrase = "resid " + str(line.resid) + " and (name HH11 or name HH12)"
+        select = universe.select_atoms(phrase)
+        if str(select) != "<AtomGroup []>":
+            junction = str(line.resid) + str(line.resname)
             ids.append(line.resid)
-            lista.append(juncao)  ## Irá guardar todos os id's que  interessam numa lista
-            dicti.update({'NH1': lista})  ## guardar com o indice desejado (neste caso trata-se do nome do atomo selecionado) com ids desejados
+            my_list.append(junction)
+            dicti.update({'NH1': my_list})
 
-# Seleção dos OD* das ASP
-
-lista = []  # necessario para limpar a lista
+# Selection of OD* from ASP
+my_list = []
 for line in interactions:
     if 'ASP' in str(line):
-        if line.resid not in lista:
-            juncao = str(line.resid) + str(line.resname)
+        if line.resid not in my_list:
+            junction = str(line.resid) + str(line.resname)
             ids.append(line.resid)
-            lista.append(juncao)
-            dicti.update({'OD*': lista})
+            my_list.append(junction)
+            dicti.update({'OD*': my_list})
 
-# Seleção dos OE* das GLU
-
-lista = []
+# Selection of  OE* from GLU
+my_list = []
 for line in interactions:
     if 'GLU' in str(line):
-        if line.resid not in lista:
-            juncao = str(line.resid) + str(line.resname)
+        if line.resid not in my_list:
+            junction = str(line.resid) + str(line.resname)
             ids.append(line.resid)
-            lista.append(juncao)
-            dicti.update({'OE*': lista})
+            my_list.append(junction)
+            dicti.update({'OE*': my_list})
 
-# Seleção dos ND1 das HIS
-
-lista = []
+# Selection of  ND1 from HIS
+my_list = []
 for line in interactions:
     if 'ND1' in str(line):
-        frase = "resid " + str(line.resid) + " and name HD1"
-        select = universe.select_atoms(frase)
-        for line1 in range(len(select)):  # apos a primeira seleção iremos necessitar de outra pois neste caso precisamos que ambos os requisitos existam
-            frase1 = "resid " + str(select[line1].resid) + " and name HE2"
-            select1 = universe.select_atoms(frase1)
-            if str(select1) != "<AtomGroup []>":  # esta funcao ira verificar se existe algo em select1, e se isso acontecer mantem-no
-                juncao = str(select1[line1].resid) + str(select1[line1].resname)
+        phrase = "resid " + str(line.resid) + " and name HD1"
+        select = universe.select_atoms(phrase)
+        for line1 in range(len(select)):
+            phrase1 = "resid " + str(select[line1].resid) + " and name HE2"
+            select1 = universe.select_atoms(phrase1)
+            if str(select1) != "<AtomGroup []>":
+                junction = str(select1[line1].resid) + str(select1[line1].resname)
                 ids.append(line.resid)
-                lista.append(juncao)
-                dicti.update({'ND1': lista})
+                my_list.append(junction)
+                dicti.update({'ND1': my_list})
 
-# Seleção dos NE2 das HIT
-
-lista = []
+# Selection of  NE2 from HIT
+my_list = []
 for line in interactions:
     if 'NE2' in str(line):
-        if line.resid not in lista:
-            juncao = str(line.resid) + str(line.resname)
+        if line.resid not in my_list:
+            junction = str(line.resid) + str(line.resname)
             ids.append(line.resid)
-            lista.append(juncao)
-            dicti.update({'NE2': lista})
+            my_list.append(junction)
+            dicti.update({'NE2': my_list})
 
-# Seleção dos HZ3 das LYS+
-
-lista = []
-lys = ['LYS', 'LYSX', 'LYSY']  # lista com lisinas existentes no gro
-for pos in lys:  # correr na lista de lys e selecionar por cada volta um dos nomes
+# Selection of  HZ3 from LYS+
+my_list = []
+lys = ['LYS', 'LYSX', 'LYSY']
+for pos in lys:
     for line in interactions:
-        if pos in str(line):  # verificar se cada nome selecionado na pos existe no ficheiro
-            frase = "resid " + str(line.resid) + " and name HZ3"
-            select = universe.select_atoms(frase)
+        if pos in str(line):
+            phrase = "resid " + str(line.resid) + " and name HZ3"
+            select = universe.select_atoms(phrase)
             if str(select) != "<AtomGroup []>":
-                juncao = str(line.resid) + str(line.resname)
+                junction = str(line.resid) + str(line.resname)
                 ids.append(line.resid)
-                lista.append(juncao)
-                dicti.update({'HZ3': lista[:-1]})
+                my_list.append(junction)
+                dicti.update({'HZ3': my_list[:-1]})
 
-lista = []
+# Selection of  OH from TYO
+my_list = []
 for line in interactions:
     if 'OH' in str(line):
-        frase = "resid " + str(line.resid) + " and name OH"
-        select = universe.select_atoms(frase)
+        phrase = "resid " + str(line.resid) + " and name OH"
+        select = universe.select_atoms(phrase)
         if str(select) != "<AtomGroup []>":
-            juncao = str(line.resid) + str(line.resname)
+            junction = str(line.resid) + str(line.resname)
             ids.append(line.resid)
-            lista.append(juncao)
-            dicti.update({'OH': lista})
+            my_list.append(junction)
+            dicti.update({'OH': my_list})
 
-lista = []
-for line in ferro:
+# Selection of FE from FE3
+my_list = []
+for line in iron:
     if 'FE3' in str(line):
-        frase = "resid " + str(line.resid) + " and name FE"
-        select = universe.select_atoms(frase)
+        phrase = "resid " + str(line.resid) + " and name FE"
+        select = universe.select_atoms(phrase)
         if str(select) != "<AtomGroup []>":
-            juncao = str(line.resid) + str(line.resname)
+            junction = str(line.resid) + str(line.resname)
             ids.append(line.resid)
-            lista.append(juncao)
-            dicti.update({'FE': lista})
+            my_list.append(junction)
+            dicti.update({'FE': my_list})
 
-
-lista = []
-for line in ferro:
+# Selection of V3 from V3
+my_list = []
+for line in iron:
     if 'V3' in str(line):
-        frase = "resid " + str(line.resid) + " and name V3"
-        select = universe.select_atoms(frase)
+        phrase = "resid " + str(line.resid) + " and name V3"
+        select = universe.select_atoms(phrase)
         if str(select) != "<AtomGroup []>":
-            juncao = str(line.resid) + str(line.resname)
+            junction = str(line.resid) + str(line.resname)
             ids.append(line.resid)
-            lista.append(juncao)
-            dicti.update({'V3': lista})
+            my_list.append(junction)
+            dicti.update({'V3': my_list})
 
-lista = []
-for line in ferro:
+# Selection of V4 from VO
+my_list = []
+for line in iron:
     if 'VO' in str(line):
-        frase = "resid " + str(line.resid) + " and name V4"
-        select = universe.select_atoms(frase)
+        phrase = "resid " + str(line.resid) + " and name V4"
+        select = universe.select_atoms(phrase)
         if str(select) != "<AtomGroup []>":
-            juncao = str(line.resid) + str(line.resname)
+            junction = str(line.resid) + str(line.resname)
             ids.append(line.resid)
-            lista.append(juncao)
-            dicti.update({'V4': lista})
+            my_list.append(junction)
+            dicti.update({'V4': my_list})
 
-lista = []
-for line in ferro:
+# Selection of V5 from VO2
+my_list = []
+for line in iron:
     if 'VO2' in str(line):
-        frase = "resid " + str(line.resid) + " and name V5"
-        select = universe.select_atoms(frase)
+        phrase = "resid " + str(line.resid) + " and name V5"
+        select = universe.select_atoms(phrase)
         if str(select) != "<AtomGroup []>":
-            juncao = str(line.resid) + str(line.resname)
+            junction = str(line.resid) + str(line.resname)
             ids.append(line.resid)
-            lista.append(juncao)
-            dicti.update({'V5': lista})
+            my_list.append(junction)
+            dicti.update({'V5': my_list})
 
-lista = []
-for line in ferro:
+# Selection of V5 from VO4
+my_list = []
+for line in iron:
     if 'VO4' in str(line):
-        frase = "resid " + str(line.resid) + " and name V5"
-        select = universe.select_atoms(frase)
+        phrase = "resid " + str(line.resid) + " and name V5"
+        select = universe.select_atoms(phrase)
         if str(select) != "<AtomGroup []>":
-            juncao = str(line.resid) + str(line.resname)
+            junction = str(line.resid) + str(line.resname)
             ids.append(line.resid)
-            lista.append(juncao)
-            dicti.update({'V5': lista})
+            my_list.append(junction)
+            dicti.update({'V5': my_list})
 
-print(dicti)
 
-'''
-Esta parte do código irá criar uma lista temporaria que sera utilizada no ficheiro python 
-'Aromaticos.py'
-'''
+"""
+This code block will create a temporary list containing all the amino acid id's used in the 
+dictionary and will be used in the python file 'Aromaticos.py'
+"""
 temp = []
 j = -1
 while j < len(ids):
@@ -168,4 +178,3 @@ while j < len(ids):
     for i in ids:
         if i not in temp:
             temp.append(i)
-
